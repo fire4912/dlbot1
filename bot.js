de259040abae32630e3bedb891c229523151b337
arafatislam <31209991+arafatislam@users.noreply.github.com>
2020-07-05 21:27:29 +0600

    Update and rename server.js to bot.js

--- bot.js
+++ bot.js
@@ -11,6 +11,3 @@ app.get("/", (request, response) => {
   response.sendStatus(200);
 });
 app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
