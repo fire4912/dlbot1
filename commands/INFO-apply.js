388358a71683e5b8af95f4b03e5497093a9a5e81
arafatislam <31209991+arafatislam@users.noreply.github.com>
2020-08-15 16:24:22 +0600

    Update INFO-apply.js

--- commands/INFO-apply.js
+++ commands/INFO-apply.js
@@ -4,7 +4,7 @@ module.exports = {
   cooldown: 3,
   execute(message) {
     message.channel.send(
-      "```Clan Requirement``````K/D : 6 ( 150 Matches )``````K/D : 5 ( 250 Matches )``````Tier : ACE ( 5200 point ) ``````Age : 13 - 30 ``````Region : Global ``````Religion : Any``````Server : ASIA``````Experience : Have to play 30 Custom Matches And 5 Tournaments. ```\n**Interested people is requested to message <#692355461894111333>**\n**Best Wishes to you** \n**- HunterYT**"
+      "```Clan Requirement``````K/D : 7 ( 150 Matches )``````K/D : 6 ( 250 Matches )``````Tier : ACE ( 5200 point ) ``````Age : 13 - 30 ``````Region : Global ``````Religion : Any``````Server : ASIA``````Experience : Have to play 30 Custom Matches And 5 Tournaments. ```\n**Interested people is requested to message <#692355461894111333>**\n**Best Wishes to you** \n**- HunterYT**"
     );
   }
 };
